23ea153ec079de6e286508229005f63598f28e06
Roman Ivanov <koppalekki@gmail.com>
2016-12-03 19:11:26 +0300

    New file Bot

--- answers.py
+++ answers.py
@@ -4,7 +4,7 @@ def get_answer(dialog, question):
 	question=input('Введите вопрос ')
 	print (dialog[question])
 		return dialog[response]
-		
+		+
 
 if __name__ == '__main__':
 	get_answer(question)
