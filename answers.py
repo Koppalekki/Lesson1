513c0e771621ec37ef07aec27791e492d9c28cef
Roman Ivanov <koppalekki@gmail.com>
2016-12-03 17:38:20 +0300

    Добавил файл

---
+++ answers.py
@@ -0,0 +1,16 @@
+if __name__ == '__main__':
+	dialog={'привет': 'И тебе привет!', 'как дела': 'Лучше всех', 'пока': 'Увидимся'}
+	answer=dialog[input('Вопрос')]
+
+
+
+
+
+	answer=dialog.get(input('type question'))
+
+
+
+	def get_answer(response):
+		response=answer
+
+	get_answer(question)
